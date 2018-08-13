5481a5026c1d5c48152bcafd40eb6a7c1c723810
atomicn50 <tinhle46@gmail.com>
2018-08-13 14:35:58 -0700

    in progress, attempted refactoring into es6 class syntax

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// TODO
+
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.groceryItems[0]}</li>
+//     <li>{props.groceryItems[1]}</li>
+//   </ul>
+// );
+
+// var GroceryList = () => (
+//     <GroceryListItem groceryItems={['Kale', 'Cucumbers']}/> // Here we are creating an instance of the `TodoList` component
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'bold' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.groceryItem}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul> 
+    {props.groceryItems.map(groceryItem => 
+      <GroceryListItem groceryItems={['Kale', 'Cucumbers']} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));cc
+
+
+
+
+
